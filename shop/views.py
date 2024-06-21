6190a7c907f966202c1c42ff12b7386c12c26fdf
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-06-21 14:46:54 +0500

    (fix): ordering

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Case, IntegerField, Value, When
 from rest_framework import generics
 
 from shop.filters import CategoryFilter, ProductFilter
@@ -6,10 +7,22 @@ from shop.serializers import CategoriesSerializer, ProductsSerializer
 
 
 class CategoriesListView(generics.ListAPIView):
-    queryset = Category.objects.filter(parent__isnull=True)
     serializer_class = CategoriesSerializer
     filterset_class = CategoryFilter
-    ordering = ["name"]
+
+    def get_queryset(self):
+        queryset = (
+            Category.objects.filter(parent__isnull=True)
+            .annotate(
+                is_english=Case(
+                    When(name__regex=r"^[a-zA-Z ]*$", then=Value(1)),
+                    default=Value(0),
+                    output_field=IntegerField(),
+                )
+            )
+            .order_by("-is_english", "name")
+        )
+        return queryset
 
 
 class CategoryDetailView(generics.RetrieveAPIView):
@@ -23,10 +36,18 @@ class CategoryDetailView(generics.RetrieveAPIView):
 
 
 class ProductsListView(generics.ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductsSerializer
     filterset_class = ProductFilter
-    ordering = ["name"]
+
+    def get_queryset(self):
+        queryset = Product.objects.annotate(
+            is_english=Case(
+                When(name__regex=r"^[a-zA-Z ]*$", then=Value(1)),
+                default=Value(0),
+                output_field=IntegerField(),
+            )
+        ).order_by("-is_english", "name")
+        return queryset
 
 
 class ProductDetailView(generics.RetrieveAPIView):
